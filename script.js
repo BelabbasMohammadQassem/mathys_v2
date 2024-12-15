d55967f19c4eb058796ac9017fac262c7e51ce68
DarkGod <qassembelabbas@gmail.com>
2024-12-15 12:50:07 +0100

    navbar et ajout du dernier partenaire

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-document.querySelectorAll('.scroll-link').forEach(link => {
-    link.addEventListener('click', function(e) {
-        e.preventDefault();
-        
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        
-        targetSection.scrollIntoView({
-            behavior: 'smooth',
-            block: 'start'
+document.addEventListener("DOMContentLoaded", function() {
+    const links = document.querySelectorAll('nav a'); // Sélectionne tous les liens dans la navbar
+
+    links.forEach(link => {
+        link.addEventListener('click', function(e) {
+            e.preventDefault(); // Empêche le comportement par défaut du lien
+            
+            const targetId = this.getAttribute('href'); // Obtient l'id de la cible
+            const targetElement = document.querySelector(targetId); // Sélectionne l'élément cible
+            
+            // Défilement vers l'élément cible
+            window.scrollTo({
+                top: targetElement.offsetTop, // Position en haut de l'élément
+                behavior: 'smooth' // Comportement de défilement
+            });
         });
     });
 });
